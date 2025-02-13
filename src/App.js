94e79e58e05c32f69f505a73c82a56ae447f798d
AdityaPrmr <parmar2100parmar@gamil.com>
2025-02-13 21:07:36 +0530

    Updated the code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRor as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home'; 
 import Login from "./Login/Login";
 import DashBoardSA from './SuperAdmin/DashBoardSA';
